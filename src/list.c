b459d99ae5bb898df7b6a4dfa8c6da91872581ca
noe <laurent.noe@univ-lille.fr>
2019-03-13 16:42:09 +0100

    Cleaning extra spaces and formating

--- src/list.c
+++ src/list.c
@@ -34,118 +34,110 @@
 
 
 /*
-* void display_queue (queue_MA *q)
-*
-* Display a queue
-*/
+ * void display_queue (queue_MA *q)
+ *
+ * Display a queue
+ */
 
-void display_queue(queue_MA * q)
-{
-
-    list_MA *l = q->first;
-    while (l) {
-      printf("|(%ld,%ld,adr=%p,blast=%ld)",
-             DIAGB(l->ma),
-             l->ma->right_pos_end,
-             (void *) l,
-             l->ma->blastscore);
-      l = l->next;
-    }
-    printf("|X\n");
-
+void display_queue(queue_MA * q) {
+  list_MA *l = q->first;
+  while (l) {
+    printf("|(%ld,%ld,adr=%p,blast=%ld)",
+           DIAGB(l->ma),
+           l->ma->right_pos_end,
+           (void *) l,
+           l->ma->blastscore);
+    l = l->next;
+  }
+  printf("|X\n");
 }
 
-void display_table(table_MA * t)
-{
-
-    long int i;
-    printf("------------------\n");
-
-    for (i = 0; i < CUSTOMER_SIZE; i++) {
-      if (t->table[i]) {
-          printf("| %p|%ld\n", (void *) t->table[i], t->table_blastscore[i]);
-      } else {
-          printf("|  NULL  |    0\n");
-      }
+void display_table(table_MA * t) {
+  long int i;
+  printf("------------------\n");
+  for (i = 0; i < CUSTOMER_SIZE; i++) {
+    if (t->table[i]) {
+      printf("| %p|%ld\n", (void *) t->table[i], t->table_blastscore[i]);
+    } else {
+      printf("|  NULL  |    0\n");
     }
-
-    printf("------------------\n");
-
+  }
+  printf("------------------\n");
 }
 
 /*
-* void new_data (tree_data *data, tree_data *data_sn, list_MA *itself)
-*
-* Fill a tree_data field (and link the list field)
-*/
+ * void new_data (tree_data *data, tree_data *data_sn, list_MA *itself)
+ *
+ * Fill a tree_data field (and link the list field)
+ */
 
 #ifdef INLINE
 inline
 #endif
-    list_MA * new_data(tree_data * data, tree_data * data_sn,
-                       list_MA * itself) {
+list_MA * new_data(tree_data * data, tree_data * data_sn,
+                   list_MA * itself) {
 
-    tree_data * next;
-    list_MA   * first;
+  tree_data * next;
+  list_MA   * first;
 
-    if (!data) {
-      return NULL;
-    }
+  if (!data) {
+    return NULL;
+  }
 
-    /* create a new list_MA */
-    first = (list_MA *)MALLOC(sizeof(list_MA));
-    ASSERT(first, "new_data()");
-    memset(first, 0, sizeof(list_MA));
-    first->ma     = itself->ma;
-    first->itself = itself;
-    first->next = NULL;
-    first->prev = NULL;
+  /* create a new list_MA */
+  first = (list_MA *)MALLOC(sizeof(list_MA));
+  ASSERT(first, "new_data()");
+  memset(first, 0, sizeof(list_MA));
+  first->ma     = itself->ma;
+  first->itself = itself;
+  first->next = NULL;
+  first->prev = NULL;
 
-    /* and a new tree_data  */
-    data->distance = DIAGB(itself->ma);
-    data->list[0] = NULL;
-    data->list[1] = NULL;
-    data->queue.first = first;
-    data->queue.last  = first;
+  /* and a new tree_data  */
+  data->distance = DIAGB(itself->ma);
+  data->list[0] = NULL;
+  data->list[1] = NULL;
+  data->queue.first = first;
+  data->queue.last  = first;
 
-    /* After that we link the lists field of the tree data structure */
-    if (data_sn) {  /* "sn" means "smallest nearest" */
+  /* After that we link the lists field of the tree data structure */
+  if (data_sn) {  /* "sn" means "smallest nearest" */
 
-      /* if a smaller element than data exists */
-      next = data_sn->list[1];
+    /* if a smaller element than data exists */
+    next = data_sn->list[1];
 
-      if (data_sn->distance > data->distance) {
+    if (data_sn->distance > data->distance) {
 
-          data->list[0] = NULL;
+      data->list[0] = NULL;
 
-          data->list[1] = data_sn;
-          data_sn->list[0] = data;
+      data->list[1] = data_sn;
+      data_sn->list[0] = data;
 
-      } else {
+    } else {
 
-          /* if data is the smallest element */
-          data->list[0] = data_sn;
-          data_sn->list[1] = data;
+      /* if data is the smallest element */
+      data->list[0] = data_sn;
+      data_sn->list[1] = data;
 
-          data->list[1] = next;
-          if (next != NULL) {
-            next->list[0] = data;
-          }
-
+      data->list[1] = next;
+      if (next != NULL) {
+        next->list[0] = data;
       }
 
     }
 
-    return first;
+  }
+
+  return first;
 
 }
 
 
 /*
-* void free_data(tree_data *data)
-*
-* Unlink the list fields of a tree_data and free the queue
-*/
+ * void free_data(tree_data *data)
+ *
+ * Unlink the list fields of a tree_data and free the queue
+ */
 
 #ifdef INLINE
 inline
@@ -167,65 +159,65 @@ void free_data(tree_data * data) {
 
 
 /*
-* list_MA * queue_push_and_sort_MA (queue_MA *q, MA *item, list_MA *itself)
-*
-* Put a MA on the top of a queue and keep the queue sorted
-*/
+ * list_MA * queue_push_and_sort_MA (queue_MA *q, MA *item, list_MA *itself)
+ *
+ * Put a MA on the top of a queue and keep the queue sorted
+ */
 
 #ifdef INLINE
 inline
 #endif
-    list_MA * queue_push_and_sort_MA(queue_MA * q,
-                                     MA * item,
-                                     list_MA * itself) {
-    list_MA *next_list_MA;
-    list_MA *curr_list_MA = (q->last);
+list_MA * queue_push_and_sort_MA(queue_MA * q,
+                                 MA * item,
+                                 list_MA * itself) {
+  list_MA *next_list_MA;
+  list_MA *curr_list_MA = (q->last);
 
-    list_MA *new_list_MA  = (list_MA *) MALLOC(sizeof(list_MA));
-    ASSERT(new_list_MA, "queue_push_and_sort_MA");
-
-
-    new_list_MA->ma     = item;
-    new_list_MA->next   = NULL;
-    new_list_MA->prev   = NULL;
-    new_list_MA->customer = NULL; /* unused in the queue */
-    new_list_MA->producer = NULL; /* unused in the queue */
-    new_list_MA->itself = itself;
+  list_MA *new_list_MA  = (list_MA *) MALLOC(sizeof(list_MA));
+  ASSERT(new_list_MA, "queue_push_and_sort_MA");
 
 
-    /* Insert "new_list_MA" at the right position in the queue */
-    while (curr_list_MA) {
-      if (curr_list_MA->ma->right_pos_end <= item->right_pos_end)
-          break;
-      curr_list_MA = curr_list_MA->prev;
-    }
+  new_list_MA->ma     = item;
+  new_list_MA->next   = NULL;
+  new_list_MA->prev   = NULL;
+  new_list_MA->customer = NULL; /* unused in the queue */
+  new_list_MA->producer = NULL; /* unused in the queue */
+  new_list_MA->itself = itself;
 
-    if (curr_list_MA) {
-      next_list_MA = curr_list_MA->next;
-      curr_list_MA->next = new_list_MA;
-    } else {
-      next_list_MA = q->first;
-      q->first = new_list_MA;
-    }
 
-    new_list_MA->prev = curr_list_MA;
-    new_list_MA->next = next_list_MA;
+  /* Insert "new_list_MA" at the right position in the queue */
+  while (curr_list_MA) {
+    if (curr_list_MA->ma->right_pos_end <= item->right_pos_end)
+      break;
+    curr_list_MA = curr_list_MA->prev;
+  }
 
-    if (next_list_MA)
-      next_list_MA->prev = new_list_MA;
-    else
-      q->last = new_list_MA;
+  if (curr_list_MA) {
+    next_list_MA = curr_list_MA->next;
+    curr_list_MA->next = new_list_MA;
+  } else {
+    next_list_MA = q->first;
+    q->first = new_list_MA;
+  }
 
-    /* return the newly created element */
-    return new_list_MA;
+  new_list_MA->prev = curr_list_MA;
+  new_list_MA->next = next_list_MA;
+
+  if (next_list_MA)
+    next_list_MA->prev = new_list_MA;
+  else
+    q->last = new_list_MA;
+
+  /* return the newly created element */
+  return new_list_MA;
 }
 
 
 /*
-* list_MA * queue_pop_MA (queue_MA *q)
-*
-* Delete the first MA of a queue
-*/
+ * list_MA * queue_pop_MA (queue_MA *q)
+ *
+ * Delete the first MA of a queue
+ */
 
 #ifdef INLINE
 inline
@@ -252,48 +244,48 @@ list_MA * queue_pop_MA(queue_MA * q) {
 
 
 /*
-* list_MA * queue_extract_MA (queue_MA *q, list_MA *l)
-*
-* extract a MA of a queue
-*/
+ * list_MA * queue_extract_MA (queue_MA *q, list_MA *l)
+ *
+ * extract a MA of a queue
+ */
 
 #ifdef INLINE
 inline
 #endif
-    list_MA * queue_extract_MA(queue_MA * q, MA * item)
+list_MA * queue_extract_MA(queue_MA * q, MA * item)
 {
 
-    list_MA *prev_list_MA;
-    list_MA *next_list_MA;
-    list_MA *curr_list_MA = q->first;
+  list_MA *prev_list_MA;
+  list_MA *next_list_MA;
+  list_MA *curr_list_MA = q->first;
 
+  if (!curr_list_MA) {
+    _WARNING("queue_extract_MA() : missing MA in the queue");
+    return NULL;
+  }
+
+  while (curr_list_MA->ma != item) {
+    curr_list_MA = curr_list_MA->next;
     if (!curr_list_MA) {
       _WARNING("queue_extract_MA() : missing MA in the queue");
       return NULL;
     }
+  }
 
-    while (curr_list_MA->ma != item) {
-      curr_list_MA = curr_list_MA->next;
-      if (!curr_list_MA) {
-          _WARNING("queue_extract_MA() : missing MA in the queue");
-          return NULL;
-      }
-    }
+  prev_list_MA = curr_list_MA->prev;
+  next_list_MA = curr_list_MA->next;
 
-    prev_list_MA = curr_list_MA->prev;
-    next_list_MA = curr_list_MA->next;
+  if (prev_list_MA)
+    prev_list_MA->next = next_list_MA;
+  else
+    q->first = next_list_MA;
 
-    if (prev_list_MA)
-      prev_list_MA->next = next_list_MA;
-    else
-      q->first = next_list_MA;
+  if (next_list_MA)
+    next_list_MA->prev = prev_list_MA;
+  else
+    q->last = prev_list_MA;
 
-    if (next_list_MA)
-      next_list_MA->prev = prev_list_MA;
-    else
-      q->last = prev_list_MA;
-
-    return curr_list_MA;
+  return curr_list_MA;
 }
 
 
@@ -303,12 +295,12 @@ inline
 #endif
 void table_MA_init(table_MA * T)
 {
-    long int i;
-    T->size = 0;
-    for (i = 0; i < CUSTOMER_SIZE; i++) {
-      T->table[i]            = NULL;
-      T->table_blastscore[i] = 0;
-    }
+  long int i;
+  T->size = 0;
+  for (i = 0; i < CUSTOMER_SIZE; i++) {
+    T->table[i]            = NULL;
+    T->table_blastscore[i] = 0;
+  }
 }
 
 
@@ -317,49 +309,49 @@ inline
 #endif
 long int table_MA_insert(table_MA * T, list_MA * insert, long int dscore, list_MA * l) {
 
-    long int curr = 0;            /* used to sort the table */
+  long int curr = 0;            /* used to sort the table */
 
-    /* if the table is already full */
-    if (T->size >= CUSTOMER_SIZE) {
-      /* if our MA has to be inserted */
-      if (dscore > T->table_blastscore[CUSTOMER_SIZE - 1]) {
-          /* we delete the MA with the lowest score */
-          list_MA *old_list = T->table[CUSTOMER_SIZE - 1];
-          T->table           [CUSTOMER_SIZE - 1] = insert;
-          T->table_blastscore[CUSTOMER_SIZE - 1] = dscore;
-          curr = CUSTOMER_SIZE - 1;
-          /* we signal to other MA the deletion */
-          if (l->producer == T)
-            table_MA_delete(old_list->customer, l);
-          if (l->customer == T)
-            table_MA_delete(old_list->producer, l);
-      } else {
-          return 0;
-      }
+  /* if the table is already full */
+  if (T->size >= CUSTOMER_SIZE) {
+    /* if our MA has to be inserted */
+    if (dscore > T->table_blastscore[CUSTOMER_SIZE - 1]) {
+      /* we delete the MA with the lowest score */
+      list_MA *old_list = T->table[CUSTOMER_SIZE - 1];
+      T->table           [CUSTOMER_SIZE - 1] = insert;
+      T->table_blastscore[CUSTOMER_SIZE - 1] = dscore;
+      curr = CUSTOMER_SIZE - 1;
+      /* we signal to other MA the deletion */
+      if (l->producer == T)
+        table_MA_delete(old_list->customer, l);
+      if (l->customer == T)
+        table_MA_delete(old_list->producer, l);
     } else {
-      /* if the table is not full */
-      T->table           [T->size] = insert;
-      T->table_blastscore[T->size] = dscore;
-      curr = T->size;
-      T->size++;
+      return 0;
     }
+  } else {
+    /* if the table is not full */
+    T->table           [T->size] = insert;
+    T->table_blastscore[T->size] = dscore;
+    curr = T->size;
+    T->size++;
+  }
 
 
-    /* table sort */
-    while (curr > 0 && T->table_blastscore[curr] > T->table_blastscore[curr - 1]) {
-      list_MA *old_list   = T->table[curr];
-      long int old_dscore = T->table_blastscore[curr];
+  /* table sort */
+  while (curr > 0 && T->table_blastscore[curr] > T->table_blastscore[curr - 1]) {
+    list_MA *old_list   = T->table[curr];
+    long int old_dscore = T->table_blastscore[curr];
 
-      T->table[curr]            = T->table[curr - 1];
-      T->table_blastscore[curr] = T->table_blastscore[curr - 1];
+    T->table[curr]            = T->table[curr - 1];
+    T->table_blastscore[curr] = T->table_blastscore[curr - 1];
 
-      T->table[curr - 1]            = old_list;
-      T->table_blastscore[curr - 1] = old_dscore;
+    T->table[curr - 1]            = old_list;
+    T->table_blastscore[curr - 1] = old_dscore;
 
-      curr--;
-    }
+    curr--;
+  }
 
-    return 1;
+  return 1;
 }
 
 
--- src/list.h
+++ src/list.h
@@ -45,9 +45,9 @@ typedef struct _table_MA {
 
 
 /*
-* List of MA structure, itself represents a pointer on the same MA
-* in another list
-*/
+ * List of MA structure, itself represents a pointer on the same MA
+ * in another list
+ */
 
 typedef struct _list_MA {
   MA *ma;
@@ -62,23 +62,23 @@ typedef struct _list_MA {
 /* Queue of MA */
 
 typedef struct _queue_MA {
-    struct _list_MA *first;
-    struct _list_MA *last;
+  struct _list_MA *first;
+  struct _list_MA *last;
 } queue_MA;
 
 
 /*
-* The data structure inside the tree, it contains the distance field which
-* represents the distance to the MA diagonale, a queue and two links which
-* point on the smaller and the bigger MA
-*/
+ * The data structure inside the tree, it contains the distance field which
+ * represents the distance to the MA diagonale, a queue and two links which
+ * point on the smaller and the bigger MA
+ */
 
 typedef struct _tree_data {
-    long int distance;
-    struct _queue_MA   queue;
-    struct _tree_data *list[2];      /* list[0] : pointer to smaller neigboor node,
-                                      * list[1] : pointer to bigger neigboor node
-                                      */
+  long int distance;
+  struct _queue_MA   queue;
+  struct _tree_data *list[2];      /* list[0] : pointer to smaller neigboor node,
+                                    * list[1] : pointer to bigger neigboor node
+                                    */
 } tree_data;
 
 
@@ -92,73 +92,73 @@ void display_queue(queue_MA * q);
 
 
 /*
-* list_MA * void new_data (tree_data *data, tree_data *data_sn, list_MA *itself)
-*
-* Fill a tree_data field (malloc a queue and link the list fiels
-*/
+ * list_MA * void new_data (tree_data *data, tree_data *data_sn, list_MA *itself)
+ *
+ * Fill a tree_data field (malloc a queue and link the list fiels
+ */
 
 list_MA *new_data(tree_data * data, tree_data * data_sn, list_MA * itself);
 
 
 /*
-* void free_data(tree_data *data)
-*
-* Unlink the list fields of a tree_data and free the queue
-*/
+ * void free_data(tree_data *data)
+ *
+ * Unlink the list fields of a tree_data and free the queue
+ */
 
 void free_data(tree_data * data);
 
 
 /*
-* list_MA * queue_push_and_sort_MA (queue_MA *q, MA *item, list_MA *itself)
-*
-* Put a MA on the top of a queue and keep the queue sorted
-*/
+ * list_MA * queue_push_and_sort_MA (queue_MA *q, MA *item, list_MA *itself)
+ *
+ * Put a MA on the top of a queue and keep the queue sorted
+ */
 
 list_MA *queue_push_and_sort_MA(queue_MA * q, MA * item, list_MA * itself);
 
 
 /*
-* list_MA * queue_pop_MA (queue_MA *q)
-*
-* Delete the first MA of a queue
-*/
+ * list_MA * queue_pop_MA (queue_MA *q)
+ *
+ * Delete the first MA of a queue
+ */
 
 list_MA *queue_pop_MA(queue_MA * q);
 
 /*
-* list_MA * queue_extract_MA (queue_MA *q, list_MA *l)
-*
-* Delete a list_MA from a queue
-*/
+ * list_MA * queue_extract_MA (queue_MA *q, list_MA *l)
+ *
+ * Delete a list_MA from a queue
+ */
 
 list_MA * queue_extract_MA(queue_MA * q, MA * item);
 
 
 /*
-* void table_MA_init (table_MA *T)
-*
-* Initializes a MA table
-*/
+ * void table_MA_init (table_MA *T)
+ *
+ * Initializes a MA table
+ */
 
 void table_MA_init(table_MA * T);
 
 
 /*
-* long int  table_MA_insert (table_MA *T, list_MA *ma, long int blastscore, list_MA *l)
-*
-* Insert a MA in a table of MA
-*/
+ * long int  table_MA_insert (table_MA *T, list_MA *ma, long int blastscore, list_MA *l)
+ *
+ * Insert a MA in a table of MA
+ */
 
 long int table_MA_insert(table_MA * T, list_MA * ma, long int blastscore,
                          list_MA * l);
 
 
 /*
-* int  table_MA_delete (table_MA *T, list_MA *ma);
-*
-* Delete a MA from a table of MA
-*/
+ * int  table_MA_delete (table_MA *T, list_MA *ma);
+ *
+ * Delete a MA from a table of MA
+ */
 
 long int table_MA_delete(table_MA * T, list_MA * ma);
 
