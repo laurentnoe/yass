984de89b724cab76d139a79ee575732d89127f86
noe <laurent.noe@univ-lille.fr>
2024-05-28 11:05:40 +0200

    Adding a new (Q&D) svg output to the dotplot script

--- yass2dotplot.php
+++ yass2dotplot.php
@@ -1,6 +1,57 @@
 #!/usr/bin/env php
 <?php
 
+/* create an image */
+function svg_imagecreate($Xdim,$Ydim) {
+    return array(
+        'Xdim' => $Xdim,
+        'Ydim' => $Ydim,
+        'SVG' => "<svg  xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"".$Xdim."\" height=\"".$Ydim."\" viewBox=\"0 0 ".$Xdim." ".$Ydim."\" overflow=\"hidden\" "
+    );
+}
+
+function svg_imagecolorallocate($img, $r_int, $g_int, $b_int) {
+    return "rgb(".$r_int.",".$g_int.",".$b_int.")";
+}
+
+function svg_imagefill(&$img,$xstart,$ystart,$col) {
+    $img["SVG"] .= " style=\"background-color:".$col."\">\n";
+}
+
+function svg_imageline(&$img ,$x1, $y1, $x2, $y2, $col, $width=1) {
+    $x1 = round($x1, 2);
+    $y1 = round($y1, 2);
+    $x2 = round($x2, 2);
+    $y2 = round($y2, 2);
+    $img["SVG"] .= " <line x1=\"".$x1."\" y1=\"".$y1."\" x2=\"".$x2."\" y2=\"".$y2."\" style=\"stroke:".$col.";stroke-width:".$width."px;stroke-linecap:square;\"/>\n";
+}
+
+function svg_imagefilledrectangle(&$img, $x, $y, $xdim, $ydim, $col) {
+    $x = round($x, 2);
+    $y = round($y, 2);
+    $img["SVG"] .= " <!--  refill before histogram -->\n";
+    $img["SVG"] .= " <rect x=\"".$x."\" y=\"".$y."\" width=\"".$xdim."\" height=\"".$ydim."\" style=\"fill:".$col."\"/>\n";
+}
+
+function svg_imagestring(&$img, $size, $x, $y, $str, $col) {
+    $x = round($x, 2);
+    $y = round($y, 2);
+    $img["SVG"] .= " <text x=\"".$x."\" y=\"".$y."\" fill=\"".$col."\" text-anchor=\"left\" dominant-baseline=\"central\">".htmlspecialchars($str)."</text>\n";
+}
+
+function svg_imagestringup(&$img, $size, $x, $y, $str, $col) {
+    $x = round($x, 2);
+    $y = round($y, 2);
+    $img["SVG"] .= " <text x=\"".$x."\" y=\"".$y."\" fill=\"".$col."\" text-anchor=\"middle\" dominant-baseline=\"central\" transform=\"rotate(90,".$x.",".$y.")\">".htmlspecialchars($str)."</text>\n";
+}
+
+function svg_imagepng(&$img,$filename) {
+    $img["SVG"] .= "</svg>\n";
+    file_put_contents($filename,$img["SVG"]);
+}
+
+
+
 /*
  * this is the yass (non-zoomable) dotplot for OFFLINE
  * scripting out of the yass output.
@@ -28,7 +79,8 @@ if ($argc < 2) {
       "                 { colorForwardLightR=192 colorForwardLightG=224 colorForwardLightB=255 }\n".
       "                 { colorReverseLightR=255 colorReverseLightG=224 colorReverseLightB=192 }\n".
       "                 { maxAl=100000 }\n".
-      " => the output is by default named \"dp.png\"\n".
+      "                 { svg=1 }\n".
+      " => the output is by default named \"dp.png\"   (or  \"dp.svg\" if you set \"svg=1\" see before) \n".
       "    (some parameters can be changed inside this script, hacks\&fixes can also be done)\n"
       );
 }
@@ -50,8 +102,8 @@ $colorForwardLightB = 255;
 $colorReverseLightR = 255;
 $colorReverseLightG = 224;
 $colorReverseLightB = 192;
-$maxAl= 1000000;
-
+$maxAl = 1000000;
+$svg = 0;
 /*
  * 1.1) Get the parameters
  */
@@ -114,6 +166,12 @@ for ($i = 2; $i < $argc; $i++) {
         case "maxAl":
           $maxAl = intval($nameval[1]);
           break;
+        case "svg":
+          $svg = intval($nameval[1]);
+          if (($svg != 0) and ((substr($filename,-4) == '.png') or substr($filename,-4) == '.Png' or (substr($filename,-4) == '.PNG'))) {
+            $filename = substr($filename, 0, -4).".svg";
+          }
+          break;
         }
       }
     }
@@ -167,7 +225,7 @@ while (!feof($fp) && $nb < $maxAl) {
     $Bmax = max($Bmax,$b2);
     $nb++;
   }
-  
+
   /* Yass second line (if given by the -d 1, not present on -d 0) looks like this :
    * "* "first_file_name" (71473 bp) / "second file_name" (1988 bp)"
    */
@@ -233,27 +291,52 @@ if (!$inverted)  {
 
 
 // Picture Allocation
-$image = ImageCreate(($dimX+$espace),($dimY+$espace));
-
+if ($svg == 0) {
+  $image =     imagecreate(($dimX+$espace),($dimY+$espace));
+} else {
+  $image = svg_imagecreate(($dimX+$espace),($dimY+$espace));
+}
 // Colors Definition
-$colBackground   = ImageColorAllocate($image, 255,255,255);
-$colForeground   = ImageColorAllocate($image,  64, 64, 64);
+if ($svg == 0) {
+  $colBackground   =     imagecolorallocate($image, 255,255,255);
+  $colForeground   =     imagecolorallocate($image,  64, 64, 64);
+  $colForward      =     imagecolorallocate($image, $colorForwardR, $colorForwardG, $colorForwardB);
+  $colForwardLight =     imagecolorallocate($image, $colorForwardLightR, $colorForwardLightG, $colorForwardLightB);
+  $colReverse      =     imagecolorallocate($image, $colorReverseR, $colorReverseG, $colorReverseB);
+  $colReverseLight =     imagecolorallocate($image, $colorReverseLightR, $colorReverseLightG, $colorReverseLightB);
+} else {
+  $colBackground   = svg_imagecolorallocate($image, 255,255,255);
+  $colForeground   = svg_imagecolorallocate($image,  64, 64, 64);
+  $colForward      = svg_imagecolorallocate($image, $colorForwardR, $colorForwardG, $colorForwardB);
+  $colForwardLight = svg_imagecolorallocate($image, $colorForwardLightR, $colorForwardLightG, $colorForwardLightB);
+  $colReverse      = svg_imagecolorallocate($image, $colorReverseR, $colorReverseG, $colorReverseB);
+  $colReverseLight = svg_imagecolorallocate($image, $colorReverseLightR, $colorReverseLightG, $colorReverseLightB);
 
-$colForward      = ImageColorAllocate($image, $colorForwardR, $colorForwardG, $colorForwardB);
-$colForwardLight = ImageColorAllocate($image, $colorForwardLightR, $colorForwardLightG, $colorForwardLightB);
-$colReverse      = ImageColorAllocate($image, $colorReverseR, $colorReverseG, $colorReverseB);
-$colReverseLight = ImageColorAllocate($image, $colorReverseLightR, $colorReverseLightG, $colorReverseLightB);
-
+}
 // Backgound Filing
-ImageFill($image, 0, 0,$colBackground);
+if ($svg == 0) {
+      imagefill($image, 0, 0,$colBackground);
+} else {
+  svg_imagefill($image, 0, 0,$colBackground);
+}
 
 // Legend Writing
 if (!$inverted) {
-  ImageStringUp($image,3,intval($dimX+60),intval($dimY/2), $filename2,$colForeground);
-  ImageString($image,3,intval($dimX/2), intval($dimY+40),$filename1,$colForeground);
+  if ($svg == 0) {
+        imagestringup($image,3,intval($dimX+60),intval($dimY/2), $filename2,$colForeground);
+        imagestring  ($image,3,intval($dimX/2), intval($dimY+60),$filename1,$colForeground);
+  } else {
+    svg_imagestringup($image,3,intval($dimX+70),intval($dimY/2), $filename2,$colForeground);
+    svg_imagestring  ($image,3,intval($dimX/2), intval($dimY+60),$filename1,$colForeground);
+  }
 } else {
-  ImageStringUp($image,3,intval($dimX+60),intval($dimY/2), $filename1,$colForeground);
-  ImageString($image,3,intval($dimX/2), intval($dimY+40),$filename2,$colForeground);
+  if ($svg == 0) {
+        imagestringup($image,3,intval($dimX+60),intval($dimY/2), $filename1,$colForeground);
+        imagestring  ($image,3,intval($dimX/2), intval($dimY+60),$filename2,$colForeground);
+  } else {
+    svg_imagestringup($image,3,intval($dimX+70),intval($dimY/2), $filename1,$colForeground);
+    svg_imagestring  ($image,3,intval($dimX/2), intval($dimY+60),$filename2,$colForeground);
+  }
 }
 
 
@@ -307,13 +390,21 @@ while (!feof($fp) && $nb < $maxAl) {
 
     // histograms computations
     if (preg_match("/f/",$format)) {
-      ImageLine($image,$x1,$y1,$x2,$y2,$colForward);
+      if ($svg == 0) {
+            imageline($image,$x1,$y1,$x2,$y2,$colForward);
+      } else {
+        svg_imageline($image,$x1,$y1,$x2,$y2,$colForward);
+      }
       for ($x = min($x1,$x2), $y = min($y1,$y2); $x < max($x1,$x2) && $y < max($y1,$y2); $x++,$y++) {
         $x_forward[$x]++;
         $y_forward[$y]++;
       }
     } else {
-      ImageLine($image,$x1,$y1,$x2,$y2,$colReverse);
+      if ($svg == 0) {
+            imageline($image,$x1,$y1,$x2,$y2,$colReverse);
+      } else {
+        svg_imageline($image,$x1,$y1,$x2,$y2,$colReverse);
+      }
       for ($x = min($x1,$x2), $y = min($y1,$y2); $x < max($x1,$x2) && $y < max($y1,$y2); $x++,$y++) {
         $x_reverse[$x]++;
         $y_reverse[$y]++;
@@ -344,15 +435,33 @@ if ($xmax < $thickness && $ymax < $thickness) {
 // Histogram plotting
 for ($x = 0; $x < $dimX; $x++) {
   if (($x_forward[$x]) * $thickness/$xmax >= 1)
-    ImageLine($image,$x,intval(1+$dimY)                                    ,$x,intval(1+$dimY+($x_forward[$x])                * $thickness/$xmax),$colForwardLight);
+    if ($svg == 0) {
+          imageline($image,$x,intval(1+$dimY)                                    ,$x,intval(1+$dimY+($x_forward[$x])                * $thickness/$xmax),$colForwardLight);
+    } else {
+      svg_imageline($image,$x,intval(1+$dimY)                                    ,$x,intval(1+$dimY+($x_forward[$x])                * $thickness/$xmax),$colForwardLight);
+    }
+
   if (($x_reverse[$x]) * $thickness/$xmax >= 1)
-    ImageLine($image,$x,intval(1+$dimY+($x_forward[$x]) * $thickness/$xmax),$x,intval(1+$dimY+($x_forward[$x]+$x_reverse[$x]) * $thickness/$xmax),$colReverseLight);
+    if ($svg == 0) {
+          imageline($image,$x,intval(1+$dimY+($x_forward[$x]) * $thickness/$xmax),$x,intval(1+$dimY+($x_forward[$x]+$x_reverse[$x]) * $thickness/$xmax),$colReverseLight);
+    } else {
+      svg_imageline($image,$x,intval(1+$dimY+($x_forward[$x]) * $thickness/$xmax),$x,intval(1+$dimY+($x_forward[$x]+$x_reverse[$x]) * $thickness/$xmax),$colReverseLight);
+    }
 }
 for ($y= 0; $y<$dimY; $y++) {
   if (($y_forward[$y]) * $thickness/$ymax >= 1)
-    ImageLine($image,intval(1+$dimX)                                    ,$y,intval(1+$dimX+($y_forward[$y])                 * $thickness/$ymax),$y,$colForwardLight);
+    if ($svg == 0) {
+          imageline($image,intval(1+$dimX)                                    ,$y,intval(1+$dimX+($y_forward[$y])                 * $thickness/$ymax),$y,$colForwardLight);
+    } else {
+      svg_imageline($image,intval(1+$dimX)                                    ,$y,intval(1+$dimX+($y_forward[$y])                 * $thickness/$ymax),$y,$colForwardLight);
+    }
+
   if (($y_reverse[$y]) * $thickness/$ymax >= 1)
-    ImageLine($image,intval(1+$dimX+($y_forward[$y]) * $thickness/$ymax),$y,intval(1+$dimX+($y_forward[$y]+$y_reverse[$y])  * $thickness/$ymax),$y,$colReverseLight);
+    if ($svg == 0) {
+          imageline($image,intval(1+$dimX+($y_forward[$y]) * $thickness/$ymax),$y,intval(1+$dimX+($y_forward[$y]+$y_reverse[$y])  * $thickness/$ymax),$y,$colReverseLight);
+    } else {
+      svg_imageline($image,intval(1+$dimX+($y_forward[$y]) * $thickness/$ymax),$y,intval(1+$dimX+($y_forward[$y]+$y_reverse[$y])  * $thickness/$ymax),$y,$colReverseLight);
+    }
 }
 
 
@@ -362,43 +471,73 @@ if (!$inverted) {
   $Xrap = $Amax/$coef;
   for ($i = 1; $i < $Xrap; $i++) {
     $reel = intval($i*$coef/$fact);
-    ImageLine($image,$reel,$dimY,$reel,$dimY+5,$colForeground);
-    ImageString($image,2,$reel,$dimY+7,$i*$coef,$colForeground);
+    if ($svg == 0) {
+          imageline($image,$reel,$dimY,$reel,$dimY+5,$colForeground);
+          imagestring($image,2,$reel,$dimY+7,$i*$coef,$colForeground);
+    } else {
+      svg_imageline($image,$reel,$dimY,$reel,$dimY+5,$colForeground);
+      svg_imagestring($image,2,$reel,$dimY+7,$i*$coef,$colForeground);
+    }
   }
 
   // Y coordinates
   $Yrap = $Bmax/$coef;
   for ($i = 1; $i < $Yrap; $i++) {
     $reel = intval($i*$coef/$fact);
-    ImageLine($image,$dimX,$reel,$dimX+5,$reel,$colForeground);
-    ImageString($image,2,$dimX+7,$reel,$i*$coef,$colForeground);
+    if ($svg == 0) {
+          imageline($image,$dimX,$reel,$dimX+5,$reel,$colForeground);
+          imagestring($image,2,$dimX+7,$reel,$i*$coef,$colForeground);
+    } else {
+      svg_imageline($image,$dimX,$reel,$dimX+5,$reel,$colForeground);
+      svg_imagestring($image,2,$dimX+7,$reel,$i*$coef,$colForeground);
+    }
   }
 } else {
   // X coordinates
   $Xrap = $Bmax/$coef;
   for ($i = 1; $i < $Xrap; $i++) {
     $reel = intval($i*$coef/$fact);
-    ImageLine($image,$reel,$dimY,$reel,$dimY+5,$colForeground);
-    ImageString($image,2,$reel,$dimY+7,$i*$coef,$colForeground);
+    if ($svg == 0) {
+          imageline($image,$reel,$dimY,$reel,$dimY+5,$colForeground);
+          imagestring($image,2,$reel,$dimY+7,$i*$coef,$colForeground);
+    } else {
+      svg_imageline($image,$reel,$dimY,$reel,$dimY+5,$colForeground);
+      svg_imagestring($image,2,$reel,$dimY+7,$i*$coef,$colForeground);
+    }
   }
 
   // Y coordinates
   $Yrap = $Amax/$coef;
   for ($i = 1; $i < $Yrap; $i++) {
     $reel = intval($i*$coef/$fact);
-    ImageLine($image,$dimX,$reel,$dimX+5,$reel,$colForeground);
-    ImageString($image,2,$dimX+7,$reel,$i*$coef,$colForeground);
+    if ($svg == 0) {
+          imageline($image,$dimX,$reel,$dimX+5,$reel,$colForeground);
+          imagestring($image,2,$dimX+7,$reel,$i*$coef,$colForeground);
+    } else {
+      svg_imageline($image,$dimX,$reel,$dimX+5,$reel,$colForeground);
+      svg_imagestring($image,2,$dimX+7,$reel,$i*$coef,$colForeground);
+    }
   }
 }
 
 /* Main Frame */
-ImageLine($image,$dimX,0,$dimX,$dimY,$colForeground);
-ImageLine($image,0,$dimY,$dimX,$dimY,$colForeground);
-ImageLine($image,0,0,0,$dimY,$colForeground);
-ImageLine($image,0,0,$dimX,0,$colForeground);
-
+if ($svg == 0) {
+      imageline($image,$dimX,0,$dimX,$dimY,$colForeground);
+      imageline($image,0,$dimY,$dimX,$dimY,$colForeground);
+      imageline($image,0,0,0,$dimY,$colForeground);
+      imageline($image,0,0,$dimX,0,$colForeground);
+} else {
+  svg_imageline($image,$dimX,0,$dimX,$dimY,$colForeground);
+  svg_imageline($image,0,$dimY,$dimX,$dimY,$colForeground);
+  svg_imageline($image,0,0,0,$dimY,$colForeground);
+  svg_imageline($image,0,0,$dimX,0,$colForeground);
+}
 
 // Save Picture as a Png file
-ImagePng($image,$filename);
+if ($svg == 0) {
+      imagepng($image,$filename);
+} else {
+  svg_imagepng($image,$filename);
+}
 echo "[done]\n";
 ?>
